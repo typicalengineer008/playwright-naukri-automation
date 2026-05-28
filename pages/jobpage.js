48ebb8de900003a4e5910f4a01aecd2498ee235f
typicalengineer008 <bishnuprasad534@gmail.com>
2026-05-29 00:50:48 +0530

    Adding the job page where i can extract information related to jobs

---
+++ pages/jobpage.js
@@ -0,0 +1,63 @@
+class JobPage {
+    constructor(page) {
+        this.page =page;
+        // this.title='//a[@class="title "]';
+        // this.companyName='//a[@class=" comp-name mw-25"]';
+        // this.experience='//span[@class="expwdth"]';
+        // this.location='//span[@class="locWdth"]';
+        this.title='a.title';
+        this.companyName='a.comp-name';
+        this.experience='span.expwdth';
+        this.location='span.locWdth';
+
+    }
+
+    async getJobTitles() {
+  await this.page.waitForSelector(this.title);
+
+    const titles = await this.page
+        .locator(this.title)
+        .allTextContents();
+
+    console.log(titles);
+
+    return titles;
+}
+async getCompanyname() {
+await this.page.waitForSelector(this.companyName);
+    const companyName = await this.page
+        .locator(this.companyName)
+        .allTextContents();
+
+    console.log(companyName);
+
+    return companyName;
+
+
+}
+async getExperience() {
+await this.page.waitForSelector(this.experience);
+    const experiencee = await this.page
+        .locator(this.experience)
+        .allTextContents();
+
+    console.log(experiencee);
+
+    return experiencee;
+
+    
+}
+async getLocation() {
+await this.page.waitForSelector(this.location);
+    const location = await this.page
+        .locator(this.location)
+        .allTextContents();
+
+    console.log(location);
+
+    return location;
+
+    
+}
+}
+module.exports={JobPage};
--- tests/JobSearch.spec.js
+++ tests/JobSearch.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/login');
-const{HomePage}=require('../pages/homepage')
+const{HomePage}=require('../pages/homepage');
+const{JobPage}=require('../pages/jobpage');
 
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ test('Naukri Jobsearch Test', async ({ page }) => {
 
     const loginPage = new LoginPage(page);
     const homepage=new HomePage(page);
+    const jobpage=new JobPage(page);
 
     await loginPage.goto();
 
@@ -23,4 +25,11 @@ test('Naukri Jobsearch Test', async ({ page }) => {
 
     await homepage.searchJob("SDET","Begaluru")
     //await page.pause();
+    await expect(page).toHaveURL(/job/);
+
+    await jobpage.getJobTitles();
+    await jobpage.getCompanyname();
+    await jobpage.getExperience();
+    await jobpage.getLocation();
+
 });
